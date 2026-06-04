4f14343db238a433caf84b43486773efe527117a
𝔦𝔰𝔩𝔞 <islanettlefold@gmail.com>
2026-06-04 10:25:47 +1000

    Update Player_CHR.gd

--- scripts/characters/Player_CHR.gd
+++ scripts/characters/Player_CHR.gd
@@ -1,8 +1,11 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+
+var speed = WALK_SPEED
+const WALK_SPEED := 5.0
+const JUMP_VELOCITY := 5.0
+const SPRINT_SPEED := 8.0
 
 
 func _physics_process(delta: float) -> void:
@@ -14,6 +17,11 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	if !Input.is_action_pressed("Sprint"):
+		speed = WALK_SPEED
+	if Input.is_action_pressed("Sprint"):
+		speed = SPRINT_SPEED
+
 	if Input.is_action_just_pressed("Pause"):
 		get_tree().quit()
 
@@ -22,10 +30,10 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("Left", "Right", "Forward", "Backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
