ead8f457fe19c1b9d1d8013e0e4e76e79e5a93e4
𝔦𝔰𝔩𝔞 <islanettlefold@gmail.com>
2026-07-07 14:18:26 +1000

    Update WeaponDatabase.gd

--- scripts/global/WeaponDatabase.gd
+++ scripts/global/WeaponDatabase.gd
@@ -11,5 +11,126 @@ const DATABASE: Dictionary = {
 			"mag_size": "",
 			"reserves": ""
 		}
-	}
+	},
+	"submachine": {
+		"name": "",
+		"scene": "",# preload("")
+		"icon": "", # preload("")
+		"stats": {
+			"body_dmg": "",
+			"head_dmg": "",
+			"mag_size": "",
+			"reserves": ""
+		}
+	},
+	"shotgun": {
+		"name": "",
+		"scene": "",# preload("")
+		"icon": "", # preload("")
+		"stats": {
+			"body_dmg": "",
+			"head_dmg": "",
+			"mag_size": "",
+			"reserves": ""
+		}
+	},
+	"sniper": {
+		"name": "",
+		"scene": "",# preload("")
+		"icon": "", # preload("")
+		"stats": {
+			"body_dmg": "",
+			"head_dmg": "",
+			"mag_size": "",
+			"reserves": ""
+		}
+	},
+	"marksman_rifle": {
+		"name": "",
+		"scene": "",# preload("")
+		"icon": "", # preload("")
+		"stats": {
+			"body_dmg": "",
+			"head_dmg": "",
+			"mag_size": "",
+			"reserves": ""
+		}
+	},
+	"assualt_rifle": {
+		"name": "",
+		"scene": "",# preload("")
+		"icon": "", # preload("")
+		"stats": {
+			"body_dmg": "",
+			"head_dmg": "",
+			"mag_size": "",
+			"reserves": ""
+		}
+	},
+	"harpoon": {
+		"name": "",
+		"scene": "",# preload("")
+		"icon": "", # preload("")
+		"stats": {
+			"body_dmg": "",
+			"head_dmg": "",
+			"mag_size": "",
+			"reserves": ""
+		}
+	},
+	"spear": {
+		"name": "",
+		"scene": "",# preload("")
+		"icon": "", # preload("")
+		"stats": {
+			"body_dmg": "",
+			"head_dmg": "",
+			"mag_size": "",
+			"reserves": ""
+		}
+	},
+	"crossbow": {
+		"name": "",
+		"scene": "",# preload("")
+		"icon": "", # preload("")
+		"stats": {
+			"body_dmg": "",
+			"head_dmg": "",
+			"mag_size": "",
+			"reserves": ""
+		}
+	},
+	"compound": {
+		"name": "",
+		"scene": "",# preload("")
+		"icon": "", # preload("")
+		"stats": {
+			"body_dmg": "",
+			"head_dmg": "",
+			"mag_size": "",
+			"reserves": ""
+		}
+	},
+	"lightmachine": {
+		"name": "",
+		"scene": "",# preload("")
+		"icon": "", # preload("")
+		"stats": {
+			"body_dmg": "",
+			"head_dmg": "",
+			"mag_size": "",
+			"reserves": ""
+		}
+	},
+	"grenade_launcher": {
+		"name": "",
+		"scene": "",# preload("")
+		"icon": "", # preload("")
+		"stats": {
+			"body_dmg": "",
+			"head_dmg": "",
+			"mag_size": "",
+			"reserves": ""
+		}
+	},
 }
